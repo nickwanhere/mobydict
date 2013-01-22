b836ca3d53c99e5b9ca8dd5b0ae033aeb217b3ed
Nick Wan <nickwanhere@gmail.com>
2013-01-23 00:56:01 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobydict::Application.config.secret_token = 'e0f9ea5a1e78b38806034fde67d96bbc5100ee9e0112e32187101c0cc1b162cd43ccf16e84c0bc1d6915e49fd998991c5e9d2fe3e8fe5a8074c862a751deca65'
